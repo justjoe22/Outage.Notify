015a2db36d537d3f257f6b19c989c294898370f0
Joe Prewitt <justjoe22@gmail.com>
2016-02-01 13:55:46 +0000

    Update GitHub One more time

--- gufire.sh
+++ gufire.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
+echo ""
 echo "Step 1 of 4: GitHub Add Files"
 git add --all
 
+echo ""
 echo "Step 2 of 4: GitHub Commit"
 echo "Commit Message: "
 read input_var
 
 git commit -m "Update GitHub $input_var"
 
+echo ""
 echo "Step 3 of 4: GitHub Push"
 read -s -p "Enter GitHub Password: " mypassword
 
 git push https://justjoe22:$mypassword@github.com/justjoe22/Outage.Notify.git --all
 
+echo ""
 echo "Step 4 of 4: Firebase Deploy"
 firebase deploy
 
