f8c35069c72cbe344372279310631e76db357bb0
Joe Prewitt <justjoe22@gmail.com>
2016-02-01 13:43:13 +0000

    Update GitHub Update 12

--- gufire.sh
+++ gufire.sh
@@ -5,7 +5,11 @@ mytimes = date +"%T"
 
 git add --all
 
-git commit -m "Update GitHub $mytimes"
+echo "Git Update Title: "
+read input_var
+echo "You entered: $input_var"
+
+git commit -m "Update GitHub $input_var"
 
 git push https://justjoe22:2Jwp0027!2010@github.com/justjoe22/Outage.Notify.git --all
 
