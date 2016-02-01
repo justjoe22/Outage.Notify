b3f1993008fcf8eb3f5741161d060efd4d001750
Joe Prewitt <justjoe22@gmail.com>
2016-02-01 13:53:45 +0000

    Update GitHub Final Script

--- gufire.sh
+++ gufire.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
+echo "Step 1 of 4: GitHub Add Files"
 git add --all
 
-echo "Git Update Title: "
+echo "Step 2 of 4: GitHub Commit"
+echo "Commit Message: "
 read input_var
-echo "You entered: $input_var"
 
 git commit -m "Update GitHub $input_var"
 
-read -s -p "Enter Password: " mypassword
+echo "Step 3 of 4: GitHub Push"
+read -s -p "Enter GitHub Password: " mypassword
 
 git push https://justjoe22:$mypassword@github.com/justjoe22/Outage.Notify.git --all
 
+echo "Step 4 of 4: Firebase Deploy"
 firebase deploy
 
+echo "Script Complete"
 # End
