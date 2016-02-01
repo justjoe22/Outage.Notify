d09160c59ab116ac6ef4eb3e0ead3c5f6e46ca82
Joe Prewitt <justjoe22@gmail.com>
2016-02-01 19:29:12 +0000

    Update GitHub Authenticate 15

--- public/assets/js/oauth.firebase.js
+++ public/assets/js/oauth.firebase.js
@@ -43,12 +43,15 @@ var ref = new Firebase("https://resplendent-inferno-4226.firebaseio.com/");
   
     var email = $('input[name=email]').val();
     var password = $('input[name=password]').val();
+    var myAuth;
     
     ref.authWithPassword({email: email, password: password},function(error, authData) {
       if (error) {
         console.log("Login Failed!", error);
       } else {
         console.log("Authenticated successfully with payload:", authData);
+        
+        myAuth = authData;
       }
     });
     
@@ -57,12 +60,12 @@ var ref = new Firebase("https://resplendent-inferno-4226.firebaseio.com/");
         url = 'my.outage.html';
 
     /* Send the data using post */
-    var posting = $.post( url, { authData: authData } );
+    var posting = $.post( url, { myAuth: myAuth } );
 
     /* Alerts the results */
     posting.done(function( data ) {
       // similar behavior as an HTTP redirect
-      //window.location.replace(url);
+      window.location.replace(url);
     });
     
   });
