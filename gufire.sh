9fbdc88de3452a02e2fc4aa201bdd1a3e7179b73
Joe Prewitt <justjoe22@gmail.com>
2016-02-01 14:47:18 +0000

    Update GitHub Spacing Issues

--- gufire.sh
+++ gufire.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-echo ""
+echo $'\n'
 echo "Step 1 of 4: GitHub Add Files"
 git add --all
 
-echo ""
+echo $'\n'
 echo "Step 2 of 4: GitHub Commit"
 echo "Commit Message: "
 read input_var
 
 git commit -m "Update GitHub $input_var"
 
-echo ""
+echo $'\n'
 echo "Step 3 of 4: GitHub Push"
 read -s -p "Enter GitHub Password: " mypassword
 
 git push https://justjoe22:$mypassword@github.com/justjoe22/Outage.Notify.git --all --quiet
 
-echo ""
+echo $'\n'
 echo "Step 4 of 4: Firebase Deploy"
 firebase deploy
 
-echo "Script Complete"
+echo $'\n'
+echo "Script Complete!"
 # End
