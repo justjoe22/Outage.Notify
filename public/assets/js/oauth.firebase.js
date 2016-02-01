f5e4a8ffaf840e4c2f14ac9d31e7bf530684164a
Joe Prewitt <justjoe22@gmail.com>
2016-02-01 19:19:31 +0000

    Update GitHub Authenticate 13

--- public/assets/js/oauth.firebase.js
+++ public/assets/js/oauth.firebase.js
@@ -1,48 +1,59 @@
 (function($) {
 
 var ref = new Firebase("https://resplendent-inferno-4226.firebaseio.com/");    
-var authClient = new FirebaseSimpleLogin(ref, function(error, user) {
-  if (error) {
-    alert(error);
-    return;
-  }
-  if (user) {
-    // User is already logged in.
-    doLogin(user);
-    
-    window.location.replace("my.outages.html");
-    
-  } else {
-    // User is logged out.
-    showLoginBox();
-  }
-});
 
-function showLoginBox(){
+// var authClient = new FirebaseSimpleLogin(ref, function(error, user) {
+//   if (error) {
+//     alert(error);
+//     return;
+//   }
+//   if (user) {
+//     // User is already logged in.
+//     doLogin(user);
+    
+//     window.location.replace("my.outages.html");
+    
+//   } else {
+//     // User is logged out.
+//     showLoginBox();
+//   }
+// });
+
+// function showLoginBox(){
 
   // Register
   $("#registerButton").on("click", function() {
+    
     var email = $('input[name=email]').val();
     var password = $('input[name=password]').val();
-    authClient.createUser(email, password, function(error,  user) {
-      if (!error) {
-        doLogin(user);
+    
+    ref.createUser({email: email, password: password}, function(error, userData) {
+      if (error) {
+        console.log("Error creating user:", error);
       } else {
-        alert(error);
+        console.log("Successfully created user account with uid:", userData.uid);
       }
     });
   });
   
   // Login 
    $("#loginButton").on("click", function() {
-    authClient.login("password", {
-      email: $('input[name=email]').val(),
-      password: $('input[name=password]').val()
+
+    var email = $('input[name=email]').val();
+    var password = $('input[name=password]').val();
+    
+    ref.authWithPassword({email: email, password: password},function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
     });
+    
   });
   
   
-}
+// }
 
 //End of Script
 })(jQuery);
