7751f6dda543a40895d40c164897f3d953a40fb2
Joe Prewitt <justjoe22@gmail.com>
2016-02-02 20:26:30 +0000

    Update GitHub Switch Site Lookup

--- public/assets/js/my.form.control.js
+++ public/assets/js/my.form.control.js
@@ -7,13 +7,15 @@
      var uid_site = "";
      var user_url = "https://resplendent-inferno-4226.firebaseio.com/users/" + myuid;
      var rUsers = new Firebase( user_url.normalize() );
-     //var rUID = rUsers.child(myuid);
-      rUsers.once("value", function(snapshot) {
-        var data = snapshot.val();
-        
-        uid_site = data.site;
-        
-      });
+     
+     rUsers.orderByKey().on("value", function(snapshot) {
+        snapshot.forEach(function(data) {
+            var message = data.val();
+            
+            uid_site = data.site;
+            
+        });
+     });
       
       //Populate Form.Init
       if (uid_site !== "") {
