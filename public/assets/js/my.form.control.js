2c6e6effb038ba89571540945b7296763766556f
Joe Prewitt <justjoe22@gmail.com>
2016-02-02 21:26:35 +0000

    Update GitHub Try again

--- public/assets/js/my.form.control.js
+++ public/assets/js/my.form.control.js
@@ -8,13 +8,8 @@
      var user_url = "https://resplendent-inferno-4226.firebaseio.com/users/" + myuid;
      var rUsers = new Firebase( user_url.normalize() );
      
-    rUsers.on("value", function(snapshot) {
-        snapshot.forEach(function(data) {
-            var message = data.val();
-            uid_site = message.site;
-        });
-    }, function (errorObject) {
-      console.log("The read failed: " + errorObject.code);
+    rUsers.once("value", function(snap) {
+      console.log(snap.val());
     });
       
       //uid_site = "bussiness1";
