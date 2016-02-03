5d23adfd04a03035c7215e9e46e7aaa7e7d40100
Joe Prewitt <justjoe22@gmail.com>
2016-02-03 16:22:14 +0000

    Update GitHub Attempt 42

--- public/assets/js/my.form.control.js
+++ public/assets/js/my.form.control.js
@@ -1,17 +1,29 @@
 (function($) {
     
-    //Default Settings OnLoad
-    $(window).load(function() {
+  //Default Settings OnLoad
+ $(window).load(function() {
     
+    waitForElement();
     
-      //uid_site = "bussiness1";
-      
+ });
+
+ //uid_site = "bussiness1";
+ function waitForElement(){
+    if(typeof uid_site !== "undefined"){
       //Populate Form.Init
       if (uid_site !== "") {
         pop_forminit(uid_site);
       }
-    
-    });
-
+      else {
+          waitForElement();
+      }
+    }
+    else{
+        setTimeout(function(){
+            waitForElement();
+        },250);
+    }
+ }
+     
 //End of Script
 })(jQuery);
