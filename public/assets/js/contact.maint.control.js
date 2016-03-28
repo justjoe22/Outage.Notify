ea12832e2cb12d97a803acd70a4153d2929c89c8
Joe Prewitt <justjoe22@gmail.com>
2016-03-28 19:18:24 +0000

    Update GitHub Starting Contact Maint.

---
+++ public/assets/js/contact.maint.control.js
@@ -0,0 +1,57 @@
+(function($) {
+
+//Default Settings OnLoad
+$(window).load(function() {
+
+  //Populate Form.Preview
+  waitForElement();
+  
+});
+
+ function waitForElement(){
+    if(typeof uid_site !== "undefined"){
+      //Populate Form.Init
+      if (uid_site !== "") {
+        pop_syslist(uid_site,"No");
+      }
+      else {
+          waitForElement();
+      }
+    }
+    else{
+        setTimeout(function(){
+            waitForElement();
+        },250);
+    }
+ }
+ 
+//Form OnSubmit
+$('form[name=system]').submit(function(event) {
+  var pub_name = $('input[name=pub_name]').val(), _
+    desc = $('input[name=desc]').val(), 
+    sysid = $('input[name=sys_id]').val();
+    
+    if (sysid!==""){
+        update_system(uid_site,pub_name,desc,sysid)
+    }
+    else {
+        sysid = add_system(uid_site,pub_name,desc,myuid);
+    }
+    
+    /* get some values from elements on the page: */
+    var $form = $( this ),
+        url = $form.attr( 'action' );
+
+    /* Send the data using post */
+    var posting = $.post( url, { sysid: sysid } );
+
+    /* Alerts the results */
+    posting.done(function( data ) {
+      // similar behavior as an HTTP redirect
+      window.location.replace(url);
+    });
+
+});
+
+//End of Script
+})(jQuery);
