614af207455b7cbfedea2b8e30b7597251dfc1b3
Joe Prewitt <justjoe22@gmail.com>
2016-07-05 17:39:27 +0000

    Update GitHub Link Auth

--- public/assets/js/oauth.firebase.js
+++ public/assets/js/oauth.firebase.js
@@ -329,69 +329,7 @@ mail_server = 'https://resplendent-inferno-4226.firebaseapp.com/';
     
   });
   
-  // Login Google
-  $( "#googleLogin" ).on("click", function(googleUser) {
-    
-   console.log('Google Auth Response', googleUser);
-    
-    // User tries to sign in to Google.
-    firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider()).catch(function(error) {
-      // An error happened.
-      if (error.code === 'auth/account-exists-with-different-credential') {
-        
-        // User's email already exists.
-        // The pending Google credential.
-        var pendingCred = error.credential;
-        
-        // The provider account's email address.
-        var email = error.email;
-        
-        // Get registered providers for this email.
-        firebase.auth().fetchProvidersForEmail(email).then(function(providers) {
-        
-          // If the user has several providers,
-          // the first provider in the list will be the "recommended" provider to use.
-          if (providers[0] === 'password') {
-            // Asks the user his password.
-            // In real scenario, you should handle this asynchronously.
-            var password = promptUserForPassword(); // TODO: implement promptUserForPassword.
-            firebase.auth().signInWithEmailAndPassword(email, password).then(function(user) {
-              return user.link(pendingCred);
-            }).then(function() {
-              // Google account successfully linked to the existing Firebase user.
-              //goToApp();
-            });
-            return;
-          }
-          // All the other cases are external providers.
-          // Construct provider object for that provider.
-          // TODO: implement getProviderForProviderId.
-          var provider = getProviderForProviderId(providers[0]);
-          // At this point, you should let the user know that he already has an account
-          // but with a different provider, and let him validate the fact he wants to
-          // sign in with this provider.
-          // Sign in to provider. Note: browsers usually block popup triggered asynchronously,
-          // so in real scenario you should ask the user to click on a "continue" button
-          // that will trigger the signInWithPopup.
-          firebase.auth().signInWithPopup(provider).then(function(result) {
-            // Remember that the user may have signed in with an account that has a different email
-            // address than the first one. This can happen as Firebase doesn't control the provider's
-            // sign in flow and the user is free to login using whichever account he owns.
-            // Step 4b.
-            // Link to Google credential.
-            // As we have access to the pending credential, we can directly call the link method.
-            result.user.link(pendingCred).then(function() {
-              // Google account successfully linked to the existing Firebase user.
-              //goToApp();
-            });
-          });
-        });
-      }
-    });
-    
-  });
-
-   // Logout
+  // Logout
   $( "#logout" ).on("click", function() {
     
     handleSignOut();
@@ -420,7 +358,18 @@ mail_server = 'https://resplendent-inferno-4226.firebaseapp.com/';
           // [END googlecredential]
           // Sign in with credential from the Google user.
           // [START authwithcred]
-          firebase.auth().signInWithCredential(credential).catch(function(error) {
+          firebase.auth().signInWithCredential(credential).then(function(result) {
+            // Remember that the user may have signed in with an account that has a different email
+            // address than the first one. This can happen as Firebase doesn't control the provider's
+            // sign in flow and the user is free to login using whichever account he owns.
+            // Step 4b.
+            // Link to Google credential.
+            // As we have access to the pending credential, we can directly call the link method.
+            result.user.link(credential).then(function() {
+              // Google account successfully linked to the existing Firebase user.
+              //goToApp();
+            });
+          }).catch(function(error) {
             // Handle Errors here.
             var errorCode = error.code;
             var errorMessage = error.message;
