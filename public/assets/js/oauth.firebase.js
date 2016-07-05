4c1d5a7e53db972131ef56eefe03c128dc6d16ca
Joe Prewitt <justjoe22@gmail.com>
2016-07-05 14:36:03 +0000

    Update GitHub Google Auth/Firebase sync

--- public/assets/js/oauth.firebase.js
+++ public/assets/js/oauth.firebase.js
@@ -388,8 +388,48 @@ mail_server = 'https://resplendent-inferno-4226.firebaseapp.com/';
     });
     
   });
-  
-      /**
+
+    // [START googlecallback]
+    function onSignIn(googleUser) {
+      console.log('Google Auth Response', googleUser);
+      // We need to register an Observer on Firebase Auth to make sure auth is initialized.
+      var unsubscribe = firebase.auth().onAuthStateChanged(function(firebaseUser) {
+        unsubscribe();
+        // Check if we are already signed-in Firebase with the correct user.
+        if (!isUserEqual(googleUser, firebaseUser)) {
+          // Build Firebase credential with the Google ID token.
+          // [START googlecredential]
+          var credential = firebase.auth.GoogleAuthProvider.credential(
+              googleUser.getAuthResponse().id_token);
+          // [END googlecredential]
+          // Sign in with credential from the Google user.
+          // [START authwithcred]
+          firebase.auth().signInWithCredential(credential).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            // The email of the user's account used.
+            var email = error.email;
+            // The firebase.auth.AuthCredential type that was used.
+            var credential = error.credential;
+            // [START_EXCLUDE]
+            if (errorCode === 'auth/account-exists-with-different-credential') {
+              alert('You have already signed up with a different auth provider for that email.');
+              // If you are using multiple auth providers on your app you should handle linking
+              // the user's accounts here.
+            } else {
+              console.error(error);
+            }
+            // [END_EXCLUDE]
+          });
+          // [END authwithcred]
+        } else {
+          console.log('User already signed-in Firebase.');
+        }
+      });
+    }
+    // [END googlecallback]
+    /**
      * Check that the given Google user is equals to the given Firebase user.
      */
     // [START checksameuser]
@@ -406,30 +446,15 @@ mail_server = 'https://resplendent-inferno-4226.firebaseapp.com/';
       }
       return false;
     }
-    
-    function onSignIn(googleUser) {
-      var profile = googleUser.getBasicProfile();
-      console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-      console.log('Name: ' + profile.getName());
-      console.log('Image URL: ' + profile.getImageUrl());
-      console.log('Email: ' + profile.getEmail());
-    }
+    // [END checksameuser]
   
    // Logout
   $( "#logout" ).on("click", function() {
     
-    firebase.auth().signOut().then(function() {
-      // Sign-out successful.
-    }, function(error) {
-      // An error happened.
-    });
-      
-    ref.unauth();
-    
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
-      console.log('User signed out.');
-    });
+    var googleAuth = gapi.auth2.getAuthInstance();
+      googleAuth.signOut().then(function() {
+        firebase.auth().signOut();
+      });
     
     window.location.replace("./");
     
